b35230b6e65523092069bc5703eea2fbb802ec7d
Atif Aziz <code@raboof.com>
2011-05-01 12:14:48 +0200

    Nested Wrapped & WrappedEqualityComparer under Lookup

--- src/Lookup.cs
+++ src/Lookup.cs
@@ -123,5 +123,52 @@ namespace System.Linq
         {
             return GetEnumerator();
         }
+
+        internal struct Wrapped<TKey>
+        {
+            public Wrapped(TKey value)
+            {
+                m_Value = value;
+            }
+
+            private readonly TKey m_Value;
+
+            public TKey Value
+            {
+                get
+                {
+                    return m_Value;
+                }
+            }
+        }
+
+        internal class WrappedEqualityComparer<TKey> : IEqualityComparer<Wrapped<TKey>>
+        {
+            private readonly IEqualityComparer<TKey> m_InnerComparer;
+
+            public WrappedEqualityComparer() : this(null) { }
+
+            public WrappedEqualityComparer(IEqualityComparer<TKey> innerComparer)
+            {
+                m_InnerComparer = innerComparer ?? EqualityComparer<TKey>.Default;
+            }
+
+            #region IEqualityComparer<Wrapped<TKey>> Members
+
+            public bool Equals(Wrapped<TKey> x, Wrapped<TKey> y)
+            {
+                return m_InnerComparer.Equals(x.Value, y.Value);
+            }
+
+            public int GetHashCode(Wrapped<TKey> obj)
+            {
+                if ((default(TKey) == null) && (obj.Value as object == null))
+                    return 0;
+
+                return m_InnerComparer.GetHashCode(obj.Value);
+            }
+
+            #endregion
+        }
     }
 }
